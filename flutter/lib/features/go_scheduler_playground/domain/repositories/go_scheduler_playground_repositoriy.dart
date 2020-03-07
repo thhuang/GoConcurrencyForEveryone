de39ca1a327928c7b6638078940cf66204e60c81
TH Huang <tzhsn.huang@gmail.com>
2020-03-08 00:14:33 +0800

    add go scheduler playground repository interface

---
+++ flutter/lib/features/go_scheduler_playground/domain/repositories/go_scheduler_playground_repositoriy.dart
@@ -0,0 +1,20 @@
+import 'dart:core';
+
+import 'package:GCFE/core/error/failures.dart';
+import 'package:GCFE/features/go_scheduler_playground/domain/entities/goroutine.dart';
+import 'package:GCFE/features/go_scheduler_playground/domain/entities/machine.dart';
+import 'package:GCFE/features/go_scheduler_playground/domain/entities/processor.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class GoSchedulerPlaygroundRepository {
+  Either<Failure, List<Goroutine>> getGlobalRunQueue();
+  Either<Failure, List<Machine>> getIdleMachines();
+  Either<Failure, List<Processor>> getAllProcessors();
+  Either<Failure, List<Machine>> getParkedMachines();
+  Failure blockGoroutine(int id);
+  Failure unblockGoroutine(int id);
+  Failure endGoroutine(int id);
+  Failure setGoroutineDuration(int id, int duration);
+  Failure progress(int duration);
+  Either<Failure, int> forkGoroutine(int id, int duration);
+}
