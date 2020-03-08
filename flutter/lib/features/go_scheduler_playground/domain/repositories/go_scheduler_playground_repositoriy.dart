b1b9bb092d3c3a75e0348ecc512a4b8ea22af2f7
TH Huang <tzhsn.huang@gmail.com>
2020-03-08 13:49:45 +0800

    refactor

--- flutter/lib/features/go_scheduler_playground/domain/repositories/go_scheduler_playground_repositoriy.dart
+++ flutter/lib/features/go_scheduler_playground/domain/repositories/go_scheduler_playground_repositoriy.dart
@@ -7,14 +7,14 @@ import 'package:GCFE/features/go_scheduler_playground/domain/entities/processor.
 import 'package:dartz/dartz.dart';
 
 abstract class GoSchedulerPlaygroundRepository {
-  Failure init();
-  Failure reset();
-  Failure progress(int duration);
+  Either<Failure, void> init();
+  Either<Failure, void> reset();
+  Either<Failure, void> progress(int duration);
   Either<Failure, int> forkGoroutine(int id, int duration);
-  Failure terminateGoroutine(int id);
-  Failure setGoroutineDuration(int id, int duration);
-  Failure blockGoroutine(int id);
-  Failure unblockGoroutine(int id);
+  Either<Failure, void> terminateGoroutine(int id);
+  Either<Failure, void> setGoroutineDuration(int id, int duration);
+  Either<Failure, void> blockGoroutine(int id);
+  Either<Failure, void> unblockGoroutine(int id);
   Either<Failure, List<Processor>> getAllProcessors();
   Either<Failure, List<Goroutine>> getGlobalRunQueue();
   Either<Failure, List<Machine>> getIdleMachines();
