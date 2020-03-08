f8d0ebe85e55115fa1f58afbadb1d8f6f80e7320
TH Huang <tzhsn.huang@gmail.com>
2020-03-08 23:42:13 +0800

    refactor

--- flutter/lib/go_concurrency_for_everyone_app.dart
+++ flutter/lib/go_concurrency_for_everyone_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'features/go_scheduler_playground/presentation/screens/go_scheduler_playground_screen.dart';
-import 'features/landing/screens/landing_screens.dart';
+import 'features/landing/presentation/screens/landing_screens.dart';
 
 class GoConcurrencyForEveryoneApp extends StatelessWidget {
   @override
