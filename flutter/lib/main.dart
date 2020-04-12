257bd0bf41f804e3f68651303ce8e52f26a37575
TH Huang <tzhsn.huang@gmail.com>
2020-04-12 10:38:40 +0800

    slower animation

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import 'go_concurrency_for_everyone_app.dart';
 
-void main() => runApp(GoConcurrencyForEveryoneApp());
+void main() {
+  timeDilation = 1.5;
+  runApp(GoConcurrencyForEveryoneApp());
+}
