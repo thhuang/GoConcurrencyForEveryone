5108f29a75a79143e61ad15f852f63313319d6de
TH Huang <tzhsn.huang@gmail.com>
2020-04-16 22:14:33 +0800

    update web

--- flutter/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-
-import 'go_concurrency_for_everyone_app.dart';
-
-void main() {
-  timeDilation = 2.0;
-  runApp(GoConcurrencyForEveryoneApp());
-}
